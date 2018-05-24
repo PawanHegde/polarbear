311b1109768034c5ad66959a429d8f38c8d66dd2
Pawan Hegde <contact@pawanhegde.com>
2018-05-24 10:14:23 +0530

    First working copy

--- src/main/java/com/pawanhegde/polarbear/model/Note.java
+++ src/main/java/com/pawanhegde/polarbear/model/Note.java
@@ -1,7 +1,45 @@
 package com.pawanhegde.polarbear.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="notes")
 public class Note {
-    public Integer id;
-    public String title;
-    public String text;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="text")
+    private String text;
+
+    public Note() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- src/main/java/com/pawanhegde/polarbear/service/NotesService.java
+++ src/main/java/com/pawanhegde/polarbear/service/NotesService.java
@@ -1,24 +1,63 @@
 package com.pawanhegde.polarbear.service;
 
 import com.pawanhegde.polarbear.model.Note;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 
 @Service
 public class NotesService {
-    private List<Note> notes = new LinkedList<>();
+    private SessionFactory sessionFactory;
 
     public List<Note> getNotes() {
-        return notes;
+        Session session = getSession();
+
+        session.beginTransaction();
+        return session.createQuery("from note", Note.class).list();
     }
 
     public Note getNote(Integer id) {
-        return notes.stream().filter(note -> note.id.equals(id)).findAny().get();
+        Session session = getSession();
+
+        session.beginTransaction();
+        Note note = session.get(Note.class, id);
+        session.close();
+
+        return note;
     }
 
     public void putNote(Note note) {
-        notes.add(note);
+        Session session = getSession();
+
+        session.beginTransaction();
+        session.persist(note);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    private Session getSession () {
+        if(sessionFactory == null) {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            properties.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            properties.setProperty("hibernate.connection.url", "jdbc:mysql://localhost/test");
+//            properties.setProperty("hibernate.id.new_generator_mappings","false");
+//            properties.setProperty("hibernate.hbm2ddl.auto","create");
+            properties.setProperty("hibernate.connection.username", "pawan");
+            properties.setProperty("hibernate.connection.password", "password");
+
+            Configuration configuration = new Configuration().setProperties(properties);
+            configuration.addAnnotatedClass(Note.class);
+
+            sessionFactory = configuration.buildSessionFactory();
+        }
+
+        return sessionFactory.openSession();
     }
 }
